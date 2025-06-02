93ff0cdefd9828261a9ba5bd4d39aaff405276ef
SomayaAhmed1430 <s.ramadan02185@dmu.edu.eg>
2025-06-02 14:47:40 +0300

    ُEdit InstructorController(insert-update-delete) & Add ViewModel

--- WebApplication2/Controllers/InstructorController.cs
+++ WebApplication2/Controllers/InstructorController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebApplication2.Models;
+using WebApplication2.ViewModels;
 
 namespace WebApplication2.Controllers
 {
@@ -25,17 +26,104 @@ namespace WebApplication2.Controllers
             return View("Details", inst);
         }
 
-        // Department/NewInstructor
+        // Instructor/NewInstructor
         public IActionResult NewInstructor()
-        { 
-            return View("NewInstructor");
+        {
+            InstructorWithDeptAndCrsListViewModel VM = new()
+            {
+                DeptList = context.Departments.ToList(),
+                CrsList = context.Courses.ToList()
+            };
+            return View("NewInstructor", VM);
         }
 
-        // Department/SaveNew? Name = value & Manger = value
-        //public IActionResult SaveNew(string name, string manger) 
-        //{
-        //    Instructor inst = new Instructor();
-        //    inst.Name = name;
-        //}
+        // Instructor/SaveNew?Name=value&Manger=value
+        public IActionResult SaveNew(Instructor InstFromReq)
+        {
+            if (InstFromReq.Name != null && InstFromReq.Img != null && InstFromReq.Salary != null && InstFromReq.Address != null && InstFromReq.DeptId != null && InstFromReq.CrsId != null)
+            {
+                context.Instructors.Add(InstFromReq);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            InstructorWithDeptAndCrsListViewModel VM = new()
+            {
+                Name = InstFromReq.Name,
+                Img = InstFromReq.Img,
+                Salary = InstFromReq.Salary,
+                Address = InstFromReq.Address,
+                DeptId = InstFromReq.DeptId,
+                CrsId = InstFromReq.CrsId,
+                DeptList = context.Departments.ToList(),
+                CrsList = context.Courses.ToList()
+            };
+            return View("NewInstructor", VM);
+        }
+
+
+        //Instructor/Edit/id
+        public IActionResult Edit(int id)
+        {
+            // collect data
+            Instructor InstFromDB = context.Instructors.FirstOrDefault(e=>e.Id==id);
+            List<Department> DeptList = context.Departments.ToList();
+            List<Course> CrsList = context.Courses.ToList();
+
+            // declare view model
+            InstructorWithDeptAndCrsListViewModel InstVM = new();
+
+            // mapping
+            InstVM.Id = InstFromDB.Id;
+            InstVM.Name = InstFromDB.Name;
+            InstVM.Img = InstFromDB.Img;
+            InstVM.Salary = InstFromDB.Salary;
+            InstVM.Address = InstFromDB.Address;
+            InstVM.DeptId = InstFromDB.DeptId;
+            InstVM.CrsId = InstFromDB.CrsId;
+            InstVM.DeptList = DeptList;
+            InstVM.CrsList = CrsList;
+
+            // return view model
+            return View("Edit", InstVM);// InstFromDB);
+        }
+
+
+        // Instructor/SaveEdit/@Model.Id
+        [HttpPost]
+        public ActionResult SaveEdit(Instructor InstFromReq) 
+        {
+            if (InstFromReq.Name != null && InstFromReq.Img != null && InstFromReq.Salary != null && InstFromReq.Address != null && InstFromReq.DeptId != null && InstFromReq.CrsId != null)
+            {
+                Instructor InstToDB = context.Instructors.FirstOrDefault(e => e.Id == InstFromReq.Id);
+                
+                InstToDB.Name = InstFromReq.Name;
+                InstToDB.Img = InstFromReq.Img;
+                InstToDB.Salary = InstFromReq.Salary;
+                InstToDB.Address = InstFromReq.Address;
+                InstToDB.DeptId = InstFromReq.DeptId;
+                InstToDB.CrsId = InstFromReq.CrsId;
+
+                context.SaveChanges();
+
+                return RedirectToAction("Index", "Instructor");
+            }
+            return View("Edit", InstFromReq);
+        }
+
+
+        // Instructor/Delete/@Model.Id
+        public IActionResult Delete(int id)
+        {
+            var inst = context.Instructors.FirstOrDefault(i => i.Id == id);
+            if (inst == null)
+                return NotFound();
+
+            context.Instructors.Remove(inst);
+            context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+
     }
 }
---
+++ WebApplication2/ViewModels/InstructorWithDeptAndCrsListViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using WebApplication2.Models;
+
+namespace WebApplication2.ViewModels
+{
+    public class InstructorWithDeptAndCrsListViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Img { get; set; }
+        public int Salary { get; set; }
+        public string Address { get; set; }
+
+        public int DeptId { get; set; }
+        public int CrsId { get; set; }
+
+
+        public List<Department> DeptList { get; set; }
+        public List<Course> CrsList { get; set; }
+    }
+}
